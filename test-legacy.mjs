36591df6bbb5739fa0ba143579d961d8e982c164
何好 <hehao@nanshecun.com>
2026-07-24 16:13:54 +0800

    移除一些无用的脚本

--- test-legacy.mjs
+++
@@ -1,5 +0,0 @@
-const dmdb = require('./out/src/server.js' ? './node_modules/dmdb' : 'dmdb');
-// Actually just test if we can detect the flag
-console.log('execArgv:', JSON.stringify(process.execArgv));
-console.log('NODE_OPTIONS:', process.env.NODE_OPTIONS);
-console.log('Has openssl-legacy:', process.execArgv.includes('--openssl-legacy-provider'));
